d72438dbb08d5d84c26e10f347777ec003983cb0
Jahns Christian Albert <vanzrael@gmail.com>
2022-11-11 07:52:18 +0700

    Improve test for validating HTTP 404

--- src/test/java/com/example/sitodo/controller/TodoListControllerTest.java
+++ src/test/java/com/example/sitodo/controller/TodoListControllerTest.java
@@ -80,7 +80,8 @@ class TodoListControllerTest {
         when(todoListService.getTodoListById(anyLong())).thenThrow(NoSuchElementException.class);
 
         mockMvc.perform(get("/list/1")).andExpectAll(
-            content().string(containsString("Not Found"))
+            content().string(containsString("Not Found")),
+            view().name("404")
         );
     }
 
